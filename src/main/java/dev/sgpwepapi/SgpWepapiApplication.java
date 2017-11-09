dfa9d652f4cda7131cb9a501a113c8ea07eb6bbb
Nickname <niko.cavaille@hotmail.fr>
2017-11-09 10:21:35 +0100

    init Spring Boot REST API

---
+++ src/main/java/dev/sgpwepapi/SgpWepapiApplication.java
@@ -0,0 +1,12 @@
+package dev.sgpwepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpWepapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpWepapiApplication.class, args);
+	}
+}
